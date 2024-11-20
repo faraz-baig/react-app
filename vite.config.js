eee747ce4557653b6621a5db32fb2a39e3d1daa0
Fraz Baig <93641958+faraz-baig@users.noreply.github.com>
2024-11-20 15:54:59 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './',
+  build: {
+    rollupOptions: {
+      input: '/src/main.jsx', // Ensure the correct path here
+    },
+  },
 })
